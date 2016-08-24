86184776b446ab508916086813d2af4d8da511e1
Kenizzer <Kenizzer@gmail.com>
2016-08-24 12:15:36 -0500

    Added scripts for ustacks and sstacks

---
+++ Scripts/dios_ustacks.sh
@@ -0,0 +1,9 @@
+####USTACKS LOOP######
+#Loop will take all fastq.gz files as inputs and run ustacks using the same parameters.
+
+(( COUNTER = 1 ))
+for i in *.fastq.gz; do
+	
+	ustacks -t gzfastq -f $i -o ../../analyses/ustacks -i $((COUNTER++)) -d -r -m -p 15 ;
+
+done
