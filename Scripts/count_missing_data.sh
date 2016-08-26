757fdee1f2553a0a8ddf6318e1ed29e759a825f8
Kenizzer <Kenizzer@gmail.com>
2016-08-26 11:52:12 -0500

    Fixed the count missing data script to calc the % missing data (using python2.x)

--- Scripts/count_missing_data.sh
+++ Scripts/count_missing_data.sh
@@ -1,8 +1,12 @@
 
-my0s=$(tail -n +3 batch_1.structure.tsv | cut -f1,2 --complement | grep -o "0" | wc -l)
-myindiv=$(tail -n +3 batch_1.structure.tsv | wc -l)
-myloci=$(head -n2 batch_1.structure.tsv | sed 's/\t/\n/g' | wc -l)
+my0s=$(tail -n +3 batch_9.structure.tsv | cut -f1,2 --complement | grep -o "0" | wc -l)
+myindiv=$(tail -n +3 batch_9.structure.tsv | wc -l)
+myloci=$(head -n2 batch_9.structure.tsv | sed 's/\t/\n/g' | wc -l)
 myobs=$(expr $myindiv \* $myloci)
+
+#Not quite sure if this floating pioint math will work on every computer, as it requires python (NOT 3)
+percent_missing=$(python -c "print float($my0s)/float($myobs)")
+echo "The is the precent missing data:"$percent_missing
 
 
 echo "This is my # of 0s:"$my0s
